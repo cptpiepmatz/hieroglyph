cd94408becedcb3c1dc2bc4079f04de115246ef4
cptpiepmatz <git+github@cptpiepmatz.de>
2024-10-08 00:11:48 +0200

    rendering of text graphemes seems to work without any flaws

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,13 @@
 use std::cmp;
-use std::{collections::HashMap, ops::Deref};
+use std::collections::HashMap;
 use std::fmt::Alignment;
+use std::ops::Deref;
 
 use ab_glyph::{Font, GlyphId, ScaleFont};
 use fonts::{NOTO_REST, NOTO_SANS, NOTO_SERIF};
-use image::{GenericImage, GenericImageView, Pixel, Primitive};
+use image::{GenericImage, GenericImageView, Pixel, Primitive, Rgba};
 use twemoji_assets::svg::SvgTwemojiAsset;
+use unicode_normalization::UnicodeNormalization;
 use unicode_segmentation::UnicodeSegmentation;
 
 type FontRef = &'static ab_glyph::FontRef<'static>;
@@ -26,9 +28,7 @@ pub struct TextGrapheme<'s> {
 
 impl<'s> TextGrapheme<'s> {
     fn new(s: &'s str) -> Self {
-        Self {
-            text: s,
-        }
+        Self { text: s }
     }
 }
 
@@ -81,26 +81,20 @@ pub enum FontOrder {
     SerifFirst,
 }
 
-pub struct GraphemesRenderer<C: Pixel> {
-    pub color: C,
+pub struct GraphemesRenderer {
+    pub color: Rgba<u8>,
     pub alignment: Alignment,
     pub scale: f32,
-    pub baseline: (u32, u32),
+    pub baseline: (i32, i32),
 
     font_order: FontOrder,
     font_cache: HashMap<char, FontRef>,
 }
 
-impl<C: Pixel> GraphemesRenderer<C> {
+impl GraphemesRenderer {
     pub fn new(font_order: FontOrder) -> Self {
         Self {
-            color: *Pixel::from_slice(&[
-                // construct black generically
-                C::Subpixel::DEFAULT_MIN_VALUE,
-                C::Subpixel::DEFAULT_MIN_VALUE,
-                C::Subpixel::DEFAULT_MIN_VALUE,
-                C::Subpixel::DEFAULT_MAX_VALUE,
-            ]),
+            color: Rgba([0, 0, 0, 255]),
             alignment: Alignment::Left,
             scale: 20.0,
             baseline: (0, 20),
@@ -112,8 +106,8 @@ impl<C: Pixel> GraphemesRenderer<C> {
 
     fn find_font(&mut self, c: char) -> (FontRef, GlyphId) {
         if let Some(font_ref) = self.font_cache.get(&c) {
-            let glyph_id = font_ref.glyph_id(c); 
-            return (font_ref, glyph_id)
+            let glyph_id = font_ref.glyph_id(c);
+            return (font_ref, glyph_id);
         }
 
         let mut font_iter = match self.font_order {
@@ -127,25 +121,27 @@ impl<C: Pixel> GraphemesRenderer<C> {
                 .chain(NOTO_REST.iter()),
         };
 
-        font_iter.find_map(|&font| {
-            let glyph_id = font.glyph_id(c);
-            match glyph_id.0 {
-                0 => None,
-                _ => Some((font.deref(), glyph_id))
-            }
-        }).unwrap_or((NOTO_SANS[0].deref(), GlyphId(0)))
+        font_iter
+            .find_map(|&font| {
+                let glyph_id = font.glyph_id(c);
+                match glyph_id.0 {
+                    0 => None,
+                    _ => Some((font.deref(), glyph_id)),
+                }
+            })
+            .unwrap_or((NOTO_SANS[0].deref(), GlyphId(0)))
     }
 
-    pub fn pixel_bb(&mut self, graphemes: Graphemes) -> (u32, u32) {
+    pub fn bounding_box(&mut self, graphemes: Graphemes) -> (f32, f32) {
         // TODO: figure out if kerning and ligatures should be considered here
 
         let (cap_font, _) = self.find_font('H');
         let cap_font = cap_font.as_scaled(self.scale);
-        let emoji_edge = cap_font.ascent().ceil() as u32;
-        
-        let mut width = 0;
-        let height = cap_font.height().ceil() as u32;
-        
+        let emoji_edge = cap_font.ascent().ceil();
+
+        let mut width = 0.0;
+        let height = cap_font.height().ceil();
+
         for grapheme in graphemes {
             match grapheme {
                 Grapheme::Emoji(_) => width += emoji_edge,
@@ -153,19 +149,76 @@ impl<C: Pixel> GraphemesRenderer<C> {
                     for c in text_grapheme.text.chars() {
                         let (font, glyph_id) = self.find_font(c);
                         let font = font.as_scaled(self.scale);
-                        width += font.h_advance(glyph_id).ceil() as u32;
+                        width += font.h_advance(glyph_id);
                     }
-                },
+                }
             }
         }
 
         (width, height)
     }
 
-    pub fn draw<I>(&mut self, graphemes: Graphemes, img: I)
+    pub fn draw<I>(&mut self, graphemes: Graphemes, img: &mut I)
     where
-        I: GenericImage<Pixel = C>
+        I: GenericImage<Pixel = Rgba<u8>>,
     {
-        todo!()
+        let y = self.baseline.1;
+        let mut x = match self.alignment {
+            Alignment::Left => self.baseline.0,
+            Alignment::Right => {
+                let width = self.bounding_box(graphemes.clone()).0;
+                self.baseline.0 - width.ceil() as i32
+            }
+            Alignment::Center => {
+                let width = self.bounding_box(graphemes.clone()).0;
+                self.baseline.0 - (width / 2.0).ceil() as i32
+            }
+        };
+
+        for grapheme in graphemes {
+            match grapheme {
+                Grapheme::Emoji(emoji_grapheme) => {
+                    dbg!(emoji_grapheme);
+                    todo!()
+                },
+                Grapheme::Text(text_grapheme) => {
+                    self.draw_text_grapheme(text_grapheme, img, &mut x, y)
+                }
+            }
+        }
+    }
+
+    fn draw_text_grapheme<I>(&mut self, grapheme: TextGrapheme, img: &mut I, x: &mut i32, y: i32)
+    where
+        I: GenericImage<Pixel = Rgba<u8>>,
+    {
+        let mut base_advance = None;
+        for c in grapheme.text.chars().nfc() {
+            let (font, glyph_id) = self.find_font(c);
+            let font = font.as_scaled(self.scale);
+            let mut glyph = font.scaled_glyph(c);
+            glyph.position.x = base_advance.unwrap_or(0.0) / 2.0;
+            if let Some(glyph) = font.outline_glyph(glyph) {
+                let px_bounds = glyph.px_bounds();
+                glyph.draw(|xg, yg, coverage| {
+                    let xi = (*x as f32 + xg as f32 + px_bounds.min.x) as u32;
+                    let yi = (y as f32 + yg as f32 + px_bounds.min.y) as u32;
+                    let mut pixel = img.get_pixel(xi, yi);
+                    let blend = self
+                        .color
+                        .map_with_alpha(|c| c, |a| (a as f32 * coverage) as u8);
+                    pixel.blend(&blend);
+                    img.put_pixel(xi, yi, pixel);
+                });
+            }
+
+            if base_advance.is_none() { 
+                base_advance = Some(font.h_advance(glyph_id)); 
+            }
+        }
+
+        if let Some(base_advance) = base_advance {
+            *x += base_advance as i32;
+        }
     }
 }
